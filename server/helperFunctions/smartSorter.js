40e7a115540e538d9e2c5c8ad7c1f2bfe194bb49
Matt Cook <mcc1985@gmail.com>
2019-02-28 23:16:17 +0000

    Search and Sort Function Implementation
    Altered the movie, book, and restaurant searches to return promises to overcome some async issues encountered in accumulating results. Also implemented some data validation to prevent passing bad matches or errors out of the function.
    
    Made a searchAll function that calls all API get functions and populates an array with the good responses received based on the user input.

--- server/helperFunctions/smartSorter.js
+++ server/helperFunctions/smartSorter.js
@@ -5,46 +5,68 @@ const request = require('request');
 require('dotenv').config();
 
 //  OMDB api call
-function showFinder(title, callback) {
+function showFinder(title) {
+  console.log("kicked off show");
   const contentTitle = title.split(' ').join('+');
   const authentication = process.env.OMDB;
   const apiLookup = `http://www.omdbapi.com/?t=${contentTitle}&apikey=${authentication}`;
   let showInfo = {};
-  request(apiLookup, (err, response, body) => {
-    showInfo = {
-      type: JSON.parse(body).Type,
-      title: JSON.parse(body).Title,
-      year: JSON.parse(body).Year,
-      imdbRating: JSON.parse(body).imdbRating,
-      response: JSON.parse(body).Response
-    };
-    console.log(JSON.parse(body).Type);
-    callback(showInfo);
+  return new Promise((resolve, reject) => {
+    request(apiLookup, (err, response, body) => {
+      if (JSON.parse(body).Response === 'True') {
+        showInfo = {
+          type: JSON.parse(body).Type,
+          title: JSON.parse(body).Title,
+          year: JSON.parse(body).Year,
+          imdbRating: JSON.parse(body).imdbRating,
+          response: true
+        };
+        resolve(showInfo);
+      }
+      reject(err);
+    });
   });
 }
 
 //  Open Library API call
-function bookFinder(title, callback) {
+async function bookFinder(title) {
+  console.log("kicked off book")
   const contentTitle = title.split(' ').join('+');
-  // const authentication = process.env.GRGET;
   const apiLookup = `http://openlibrary.org/search.json?q=${contentTitle}`;
   let bookInfo = {};
-  request(apiLookup, (err, response, body) => {
-    if (title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
-      bookInfo = {
-        type: 'book',
-        title: JSON.parse(body).docs[0].title,
-        author: JSON.parse(body).docs[0].author_name[0],
-        year: JSON.parse(body).docs[0].first_publish_year
-
-      };
-    }
-    callback(bookInfo);
+  return new Promise((resolve, reject) => {
+    request(apiLookup, (err, response, body) => {
+      if (JSON.parse(body).num_found !== 0 && title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
+        bookInfo = {
+          type: 'book',
+          title: JSON.parse(body).docs[0].title,
+          author: JSON.parse(body).docs[0].author_name[0],
+          year: JSON.parse(body).docs[0].first_publish_year,
+          response: true
+        };
+        resolve(bookInfo);
+      }
+      reject(err);
+    });
   });
+
+  // await request(apiLookup, (err, response, body) => {
+  //   if (title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
+  //     bookInfo = {
+  //       type: 'book',
+  //       title: JSON.parse(body).docs[0].title,
+  //       author: JSON.parse(body).docs[0].author_name[0],
+  //       year: JSON.parse(body).docs[0].first_publish_year,
+  //       response: true
+  //     };
+  //     return bookInfo;
+  //   }
+  // });
 }
 
 //  Yelp API call
-function restoFinder(title, callback) {
+async function restoFinder(title, callback) {
+  console.log("kicked off resto")
   const restoTitle = title.split(' ').join('-').toLowerCase();
   const apiLookup = {
     url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
@@ -52,47 +74,68 @@ function restoFinder(title, callback) {
       Authorization: `Bearer ${process.env.YELPAPI}`
     }
   };
-  console.log(apiLookup.url);
   let restoInfo = {};
-  request(apiLookup, (err, response, body) => {
-    restoInfo = JSON.parse(body).businesses[0];
-    callback(restoInfo);
+  return new Promise((resolve, reject) => {
+    request(apiLookup, (err, response, body) => {
+      if (JSON.parse(body).total !== 0 && title === JSON.parse(body).businesses[0].name.slice(0, title.length)) {
+        restoInfo = {
+          type: 'toEat',
+          name: JSON.parse(body).businesses[0].name,
+          yelpRating: JSON.parse(body).businesses[0].rating,
+          price: JSON.parse(body).businesses[0].price,
+          address: JSON.parse(body).businesses[0].location.display_address.join(' '),
+          response: true
+        };
+        resolve(restoInfo);
+      }
+      reject(err);
+    });
   });
 }
 
-//  API call
-function buyFinder(title, callback) {
-  const restoTitle = title.split(' ').join('-').toLowerCase();
-  const apiLookup = {
-    url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
-    headers: {
-      Authorization: `Bearer ${process.env.YELPAPI}`
-    }
-  };
-  console.log(apiLookup.url);
-  let restoInfo = {};
-  request(apiLookup, (err, response, body) => {
-    restoInfo = JSON.parse(body).businesses[0];
-    callback(restoInfo);
-  });
-}
-
-// search/?location=Toronto&radius=10000&categories=restaurants&term=${restoTitle}
-
-// function smartSorter(userTitle, callback) {
-//   const searchHits = [];
-//   //  Check if exists shows
-//   showFinder(userTitle, (resObj) => {
-//     if (resObj.response === 'True') {
-//       console.log(searchHits);
+//  Retail API call
+// function buyFinder(title, callback) {
+//   const restoTitle = title.split(' ').join('-').toLowerCase();
+//   const apiLookup = {
+//     url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
+//     headers: {
+//       Authorization: `Bearer ${process.env.YELPAPI}`
 //     }
+//   };
+//   console.log(apiLookup.url);
+//   let restoInfo = {};
+//   request(apiLookup, (err, response, body) => {
+//     restoInfo = JSON.parse(body).businesses[0];
+//     callback(restoInfo);
 //   });
-//   callback(searchHits)
 // }
 
-restoFinder('Insomnia', (obj) => {
-  console.log(obj);
-});
+async function searchAll(userInput, callback) {
+  const searchHits = [];
+
+  let showResult = await showFinder(userInput).then().catch(err => console.log(err));
+  let bookResult = await bookFinder(userInput).then().catch(err => console.log(err));
+  let restoResult = await restoFinder(userInput).then().catch(err => console.log(err));
+  if (showResult) {
+    searchHits.push(showResult);
+  }
+  if (bookResult) {
+    searchHits.push(bookResult);
+  }
+  if (restoResult) {
+    searchHits.push(restoResult);
+  }
+  console.log(searchHits);
+  //callback(searchHits);
+}
+
+searchAll('To Kill a Mockingbird');
+//   , (obj) => {
+//   console.log(obj);
+// });
+// showFinder('Big Mouth', (obj) => {
+//   console.log(obj);
+// });
 
 // //  Google Books api call
 // function bookFinder(title, callback) {
