46118fcd41d56241ea13c3b2802a49de369caabd
Halimo <halimoghelle@gmail.com>
2019-02-28 21:14:02 +0000

    set up ejs files and set up basic routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,27 @@ const PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set("view engine", "ejs")
+app.use(cookieSession({
+  name: 'session',
+  keys: ["TinyAppisanappabouturlshortners"],
+}))
+
+
+app.get("/login/:id", (req, res) => {
+ req.session.id = req.params.id;
+  res.redirect("/")
+
+});
+app.get("/", (req, res) => {
+
+  res.render("home_page");
+});
+
+app.get("/towatch", (req, res) => {
+ 
+      res.render("list_page");
+});
 
 app.listen(PORT, () => {
   console.log('listening on ', PORT);
