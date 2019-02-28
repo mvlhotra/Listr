a15043c613ab5fdd22eeaf174e4f398ab05baef1
Matt Cook <mcc1985@gmail.com>
2019-02-28 19:02:47 +0000

    Smart Sorter Function Updated
    The smart sorter now has a working function for searching Toronto restaurants using the Yelp API. It also has a function for searching books on the Open Library API, which works for some well know books but is a bit hit or miss.

--- server/helperFunctions/smartSorter.js
+++ server/helperFunctions/smartSorter.js
@@ -5,18 +5,111 @@ const request = require('request');
 require('dotenv').config();
 
 //  OMDB api call
-function showFinder(searchItem, callback) {
-  const contentTitle = searchItem.split(' ').join('+');
+function showFinder(title, callback) {
+  const contentTitle = title.split(' ').join('+');
   const authentication = process.env.OMDB;
   const apiLookup = `http://www.omdbapi.com/?t=${contentTitle}&apikey=${authentication}`;
+  let showInfo = {};
   request(apiLookup, (err, response, body) => {
-    callback(body);
+    showInfo = {
+      type: JSON.parse(body).Type,
+      title: JSON.parse(body).Title,
+      year: JSON.parse(body).Year,
+      imdbRating: JSON.parse(body).imdbRating,
+      response: JSON.parse(body).Response
+    };
+    console.log(JSON.parse(body).Type);
+    callback(showInfo);
   });
 }
 
-function smartSorter(searchItem, callback) {
+//  Open Library API call
+function bookFinder(title, callback) {
+  const contentTitle = title.split(' ').join('+');
+  // const authentication = process.env.GRGET;
+  const apiLookup = `http://openlibrary.org/search.json?q=${contentTitle}`;
+  let bookInfo = {};
+  request(apiLookup, (err, response, body) => {
+    if (title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
+      bookInfo = {
+        type: 'book',
+        title: JSON.parse(body).docs[0].title,
+        author: JSON.parse(body).docs[0].author_name[0],
+        year: JSON.parse(body).docs[0].first_publish_year
 
+      };
+    }
+    callback(bookInfo);
+  });
+}
 
-smartSorter('Big Mouth', (obj) => {
+//  Yelp API call
+function restoFinder(title, callback) {
+  const restoTitle = title.split(' ').join('-').toLowerCase();
+  const apiLookup = {
+    url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
+    headers: {
+      Authorization: `Bearer ${process.env.YELPAPI}`
+    }
+  };
+  console.log(apiLookup.url);
+  let restoInfo = {};
+  request(apiLookup, (err, response, body) => {
+    restoInfo = JSON.parse(body).businesses[0];
+    callback(restoInfo);
+  });
+}
+
+//  API call
+function buyFinder(title, callback) {
+  const restoTitle = title.split(' ').join('-').toLowerCase();
+  const apiLookup = {
+    url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
+    headers: {
+      Authorization: `Bearer ${process.env.YELPAPI}`
+    }
+  };
+  console.log(apiLookup.url);
+  let restoInfo = {};
+  request(apiLookup, (err, response, body) => {
+    restoInfo = JSON.parse(body).businesses[0];
+    callback(restoInfo);
+  });
+}
+
+// search/?location=Toronto&radius=10000&categories=restaurants&term=${restoTitle}
+
+// function smartSorter(userTitle, callback) {
+//   const searchHits = [];
+//   //  Check if exists shows
+//   showFinder(userTitle, (resObj) => {
+//     if (resObj.response === 'True') {
+//       console.log(searchHits);
+//     }
+//   });
+//   callback(searchHits)
+// }
+
+restoFinder('Insomnia', (obj) => {
   console.log(obj);
 });
+
+// //  Google Books api call
+// function bookFinder(title, callback) {
+//   const contentTitle = title.split(' ').join('+');
+//   const authentication = process.env.GOOGLE;
+//   const apiLookup = `https://www.googleapis.com/books/v1/volumes?q=${contentTitle}&key=${authentication}`;
+//   let bookInfo = {};
+//   request(apiLookup, (err, response, body) => {
+//     bookInfo = JSON.parse(body);
+//     // {
+//     //   type: JSON.parse(body).Type,
+//     //   title: JSON.parse(body).Title,
+//     //   year: JSON.parse(body).Year,
+//     //   imdbRating: JSON.parse(body).imdbRating,
+//     //   response: JSON.parse(body).Response
+//     // };
+//     // console.log(JSON.parse(body).Type);
+//     callback(bookInfo);
+//   });
+// }
