fcfb154811a1d1c5ad0acb6284e8616efce61571
Matt Cook <mcc1985@gmail.com>
2019-03-01 23:06:50 +0000

    Add helper function for inserting data. It has not been tested

--- helpers/Db_Queries.js
+++ helpers/Db_Queries.js
@@ -40,11 +40,46 @@ module.exports = (knex) => {
     });
   }
 
-  function insertEat()
+  function getListID(userID, cat) {
+    return new Promise((resolve, reject) => {
+      knex('list').select(list.id)
+        .where({ 'list.user_id': data.userID })
+        .where({ 'list.cat_code': data.cat })
+        .then ((list) => {
+          return resolve(list_id);
+        })
+        .catch((error) => reject(error));
+    });
+  }
+
+  function getItemID() {
+    return new Promise((resolve, reject) => {
+      knex('item').select(max(item.id))
+        .then((item) => {
+          return resolve(item_id);
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  // HASNT BEEN TESTED
+  async function insertItem(data) {
+    knex('item').insert({ cat_code: data.cat, item_name: data.title });
+    const listID = await getListID(data.userID, data.cat);
+    const itemID = await getItemID();
+    knex('list_item').insert({ list_id: listID[0], item_id: itemID[0] });
+  }
 
   return {
     count: countList,
     findByEmail: findUserByEmail,
-    findByID: findUserByID
+    findByID: findUserByID,
+    insert: insertItem
   };
 };
+
+/* data = {
+  cat: 'WAT',
+  title: 'Batman',
+  userID: 2
+  }
+*/
