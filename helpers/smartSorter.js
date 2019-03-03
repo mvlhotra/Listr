8092093f7b3358540be1c012e767f66d77692669
Matt Cook <mcc1985@gmail.com>
2019-03-03 19:32:08 +0000

    Connected smartSort Function to Front End
    Peer code with Halimo. Using jquery/ajax when an item is 'added' from the main page that item name (and associated user) is passed to a sorter route which calls the smartSort function. When a single match is found and returned, the item is automatically inserted into the database under that category and the count is updated on the page. When no matches are found that result is announced. When multiple matches are found, the matches are sent back to the front end as an array of objects.

--- helpers/smartSorter.js
+++ helpers/smartSorter.js
@@ -1,118 +1,127 @@
-//  JS function to take inputted list items from user and
-//  determine their type and return JSON object of relevant data
-const request = require('request');
-require('dotenv').config();
+module.exports = () => {
+  //  JS function to take inputted list items from user and
+  //  determine their type and return JSON object of relevant data
+  const request = require('request');
+  require('dotenv').config();
 
-//  OMDB api call
-function watchFinder(title) {
-  const contentTitle = title.split(' ').join('+');
-  const authentication = process.env.OMDB;
-  const apiLookup = `http://www.omdbapi.com/?t=${contentTitle}&apikey=${authentication}`;
-  let watchInfo = {};
-  return new Promise((resolve, reject) => {
-    request(apiLookup, (err, response, body) => {
-      if (JSON.parse(body).Response === 'True') {
-        // console.log(JSON.parse(body));
-        watchInfo = {
-          type: 'WAT',
-          title: JSON.parse(body).Title,
-          year: JSON.parse(body).Year,
-          imdbRating: JSON.parse(body).imdbRating,
-          response: true
-        };
-        resolve(watchInfo);
-      }
-      reject(err);
+  //  OMDB api call
+  function watchFinder(title) {
+    const contentTitle = title.split(' ').join('+');
+    const authentication = process.env.OMDB;
+    const apiLookup = `http://www.omdbapi.com/?t=${contentTitle}&apikey=${authentication}`;
+    let watchInfo = {};
+    return new Promise((resolve, reject) => {
+      request(apiLookup, (err, response, body) => {
+        if (JSON.parse(body).Response === 'True') {
+          // console.log(JSON.parse(body));
+          watchInfo = {
+            type: 'WAT',
+            name: JSON.parse(body).Title,
+            year: JSON.parse(body).Year,
+            imdbRating: JSON.parse(body).imdbRating,
+            response: true
+          };
+          resolve(watchInfo);
+        }
+        reject(err);
+      });
     });
-  });
-}
+  }
 
-//  Open Library API call
-function readFinder(title) {
-  const contentTitle = title.split(' ').join('+');
-  const apiLookup = `http://openlibrary.org/search.json?q=${contentTitle}`;
-  let readInfo = {};
-  return new Promise((resolve, reject) => {
-    request(apiLookup, (err, response, body) => {
-      if (JSON.parse(body).num_found !== 0 && title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
-        readInfo = {
-          type: 'REA',
-          title: JSON.parse(body).docs[0].title,
-          author: JSON.parse(body).docs[0].author_name[0],
-          year: JSON.parse(body).docs[0].first_publish_year,
-          response: true
-        };
-        resolve(readInfo);
-      }
-      reject(err);
+  //  Open Library API call
+  function readFinder(title) {
+    const contentTitle = title.split(' ').join('+');
+    const apiLookup = `http://openlibrary.org/search.json?q=${contentTitle}`;
+    let readInfo = {};
+    return new Promise((resolve, reject) => {
+      request(apiLookup, (err, response, body) => {
+        if (JSON.parse(body).num_found !== 0 && title.toLowerCase() === JSON.parse(body).docs[0].title.toLowerCase()) {
+          readInfo = {
+            type: 'REA',
+            name: JSON.parse(body).docs[0].title,
+            author: JSON.parse(body).docs[0].author_name[0],
+            year: JSON.parse(body).docs[0].first_publish_year,
+            response: true
+          };
+          resolve(readInfo);
+        }
+        reject(err);
+      });
     });
-  });
-}
+  }
 
-//  Yelp API call
-function eatFinder(title) {
-  const restoTitle = title.split(' ').join('-').toLowerCase();
-  const apiLookup = {
-    url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
-    headers: {
-      Authorization: `Bearer ${process.env.YELPAPI}`
+  //  Yelp API call
+  function eatFinder(title) {
+    const restoTitle = title.split(' ').join('-').toLowerCase();
+    const apiLookup = {
+      url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
+      headers: {
+        Authorization: `Bearer ${process.env.YELPAPI}`
+      }
+    };
+    let eatInfo = {};
+    return new Promise((resolve, reject) => {
+      request(apiLookup, (err, response, body) => {
+        if (JSON.parse(body).total !== 0 && title === JSON.parse(body).businesses[0].name.slice(0, title.length)) {
+          eatInfo = {
+            type: 'EAT',
+            name: JSON.parse(body).businesses[0].name,
+            yelpRating: JSON.parse(body).businesses[0].rating,
+            price: JSON.parse(body).businesses[0].price,
+            address: JSON.parse(body).businesses[0].location.display_address.join(' '),
+            response: true
+          };
+          resolve(eatInfo);
+        }
+        reject(err);
+      });
+    });
+  }
+
+  //  Retail API call
+  // function buyFinder(title, callback) {
+  //   const restoTitle = title.split(' ').join('-').toLowerCase();
+  //   const apiLookup = {
+  //     url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.YELPAPI}`
+  //     }
+  //   };
+  //   console.log(apiLookup.url);
+  //   let restoInfo = {};
+  //   request(apiLookup, (err, response, body) => {
+  //     restoInfo = JSON.parse(body).businesses[0];
+  //     callback(restoInfo);
+  //   });
+  // }
+
+  async function searchAll(userInput, callback) {
+    const searchHits = [];
+
+    const watchResult = await watchFinder(userInput).then().catch(err => console.log(err));
+    const readResult = await readFinder(userInput).then().catch(err => console.log(err));
+    const eatResult = await eatFinder(userInput).then().catch(err => console.log(err));
+    if (watchResult) {
+      searchHits.push(watchResult);
     }
+    if (readResult) {
+      searchHits.push(readResult);
+    }
+    if (eatResult) {
+      searchHits.push(eatResult);
+    }
+    return searchHits;
+  }
+
+  return {
+    watch: watchFinder,
+    read: readFinder,
+    eat: eatFinder,
+    // buy: buyFinder,
+    search: searchAll
   };
-  let eatInfo = {};
-  return new Promise((resolve, reject) => {
-    request(apiLookup, (err, response, body) => {
-      if (JSON.parse(body).total !== 0 && title === JSON.parse(body).businesses[0].name.slice(0, title.length)) {
-        eatInfo = {
-          type: 'EAT',
-          name: JSON.parse(body).businesses[0].name,
-          yelpRating: JSON.parse(body).businesses[0].rating,
-          price: JSON.parse(body).businesses[0].price,
-          address: JSON.parse(body).businesses[0].location.display_address.join(' '),
-          response: true
-        };
-        resolve(eatInfo);
-      }
-      reject(err);
-    });
-  });
-}
-
-//  Retail API call
-// function buyFinder(title, callback) {
-//   const restoTitle = title.split(' ').join('-').toLowerCase();
-//   const apiLookup = {
-//     url: `https://api.yelp.com/v3/businesses/search?location=toronto&categories=restaurants,all&term=${restoTitle}&sort_by=best_match`,
-//     headers: {
-//       Authorization: `Bearer ${process.env.YELPAPI}`
-//     }
-//   };
-//   console.log(apiLookup.url);
-//   let restoInfo = {};
-//   request(apiLookup, (err, response, body) => {
-//     restoInfo = JSON.parse(body).businesses[0];
-//     callback(restoInfo);
-//   });
-// }
-
-async function searchAll(userInput, callback) {
-  const searchHits = [];
-
-  const watchResult = await watchFinder(userInput).then().catch(err => console.log(err));
-  const readResult = await readFinder(userInput).then().catch(err => console.log(err));
-  const eatResult = await eatFinder(userInput).then().catch(err => console.log(err));
-  if (watchResult) {
-    searchHits.push(watchResult);
-  }
-  if (readResult) {
-    searchHits.push(readResult);
-  }
-  if (eatResult) {
-    searchHits.push(eatResult);
-  }
-  console.log(searchHits);
-}
-
-searchAll('Tacos El Asador');
+};
+// searchAll('Tacos El Asador');
 
 // //  Google Books api call
 // function bookFinder(title, callback) {
--- public/scripts/indexListCount.js
+++ public/scripts/indexListCount.js
@@ -11,10 +11,67 @@ $(document).ready(function () {
       $(`#${category[0]}Count`).html(category[1].toString());
     });
   }
+  function updatePageCounts() {
+    const user = $('#EATCount').data('id');
+    $.ajax(`/api/listCounts/${user}`, { method: 'GET' }).then(function (content) {
+      listCounts = getListCounts(content);
+      updateCounts(listCounts);
+    });
+  }
 
-  const user = $('#EATCount').data('id');
-  $.ajax(`/api/listCounts/${user}`, { method: 'GET' }).then(function (content) {
-    listCounts = getListCounts(content);
-    updateCounts(listCounts);
-  });
+  updatePageCounts();
+
+
+  $('.smart-search').submit(function (event) {
+    event.preventDefault();
+    let searchHits;
+    const $newItemName = $('.smart-search .user-item');
+    $.post('/sorter', { item: $newItemName.val(), user: $('#EATCount').data('id') })
+      .done(function(data) {
+        if (data.status === 201) {
+          $newItemName.val('').focus();
+          updatePageCounts();
+        } else if (data === 'No match found') {
+          $newItemName.val('').focus();
+          alert(data);
+        } else {
+          $newItemName.val('').focus();
+          console.log(data);
+          // Lightbox code here
+        }
+      });
+    });
+      // $.ajax({
+      // url: '/sorter',
+      // type: 'POST',
+      // data: {
+      //   item: newItemName.val()
+      // },
+      // success: (function(data) {
+      //   console.log(data)
+      // });
+    // if (searchHits.length === 1) {
+    //   console.log(searchHits);
+    //   $.post(`/lists/${searchHits[0].type}`, searchHits[0].name)
+    //     .done(updatePageCounts());
+    // }
+  // });
+
+    // $postTweet.submit(function (ev) {
+  //   ev.preventDefault();
+  //   $('.errorMsg').slideUp(50);
+  //   if ($tweetText.val() === '' || $tweetText.val() === null) {
+  //     $('.errorMsg').html('Blank tweets are not accepted!');
+  //     $('.errorMsg').slideDown(400);
+  //   } else if ($tweetText.val().length > 140) {
+  //     $('.errorMsg').html('Too many characters!');
+  //     $('.errorMsg').slideDown(400);
+  //   } else {
+  //     $.post('/tweets', $postTweet.serialize())
+  //       .done(loadTweets);
+  //     $tweetText.val('');
+  //     $counter.html(140);
+  //   }
+  // });
+
 });
