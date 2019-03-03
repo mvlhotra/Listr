5d89cae01941421d0424bea179a3d43ef7690624
Nik Malhotra <nmal@me.com>
2019-03-03 20:22:19 +0000

    added checkmark trigger

--- public/scripts/displayLists.js
+++ public/scripts/displayLists.js
@@ -5,14 +5,29 @@ $(document).ready(function () {
 
 
   function formatListItems(items) {
-    items.forEach(item => {
-      $('.list-group').append(`<li class="list-group-item"><i class="fas fa-check"> </i>${item.item_name}<i class="fas fa-times"></i>`);
+    items.forEach((item) => {
+      $('.list-group').append(`<li class="list-group-item" data-itemId =${item.id}><i class="fas fa-check"></i> ${item.item_name}<span class="edit-delete"><a href=""><i class="fas fa-pencil-alt"></i></a><a href=""><i class="fas fa-times"></i></a></span>`);
     });
   }
 
-  $.ajax(`/api/lists/${cat_code}/${userId}`, { method: 'GET' }).then(function (content) {
-    formatListItems(content);
-    $('#test').fadeIn(1000);
-  });
+  function getListItems() {
+    $.ajax(`/api/lists/${cat_code}/${userId}`, { method: 'GET' }).then(function (content) {
+      formatListItems(content);
+    });
+  }
 
+  $('.list-group').fadeIn();
+
+  $('.list-group').on('click', '.fas.fa-check', function (done) {
+    const checkedItemId = $(this).parent($(done.target)).attr('data-itemId');
+    $.ajax({
+      type: 'POST',
+      url: 'something',
+      data: checkedItemId
+    }).done(function () { getListItems(); });
+
+  });
+  getListItems();
 });
+
+
